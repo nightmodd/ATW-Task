dabf1f5ecb76cb970b67f2a50658235abf25331b
Mostafa Ghareeb <mostafaghareeb132@gmail.com>
2024-05-25 19:32:50 +0300

    fix: fix bug in responsive navbarstyles
    Signed-off-by: Mostafa Ghareeb <mostafaghareeb132@gmail.com>

--- first-task/components/ResponsiveNavbar.vue
+++ first-task/components/ResponsiveNavbar.vue
@@ -40,6 +40,9 @@ nav,
 @media only screen and (max-width: 56.25rem) {
   nav {
     position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
     display: flex;
     justify-content: space-between;
     align-items: center;
